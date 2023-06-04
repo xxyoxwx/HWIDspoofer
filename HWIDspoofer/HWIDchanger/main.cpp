4d485914c57aebbb385f5131b730995f4c65d829
xxyoxwx <105009376+xxyoxwx@users.noreply.github.com>
2023-06-04 20:02:50 +0200

    Update main.cpp

--- HWIDspoofer/HWIDchanger/main.cpp
+++ HWIDspoofer/HWIDchanger/main.cpp
@@ -19,6 +19,7 @@ std::wstring FindDeviceName() {
     std::wstring deviceName;
     HDEVINFO deviceInfoSet = SetupDiGetClassDevs(NULL, L"USB", NULL, DIGCF_PRESENT | DIGCF_ALLCLASSES);
     if (deviceInfoSet != INVALID_HANDLE_VALUE) {
+    # TO COE KEKE
         SP_DEVINFO_DATA deviceInfoData = {};
         deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
         DWORD index = 0;
@@ -41,24 +42,24 @@ std::wstring FindDeviceName() {
     }
     return deviceName;
 }
-// This function extracts the current hardware serial number string
+
 std::string GetCurrentSerialNumber() {
-    // Declare variables
+
     DWORD dwSize = 0;
     char buffer[1024];
     std::string serialNumber;
 
-    // Open the device handle to the device
+
     HANDLE hDevice = CreateFile(DEVICE_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
     if (hDevice == INVALID_HANDLE_VALUE) {
-        // Could not open device
+
         return serialNumber;
     }
 
-    // Query for the size of the serial number buffer
+
     DeviceIoControl(hDevice, MY_DEVICE_IOCTL, NULL, 0, &dwSize, sizeof(DWORD), NULL, NULL);
 
-    // Allocate a buffer of the required size
+
     char* lpBuffer = new char[dwSize];
     if (!lpBuffer) {
         // Could not allocate buffer
@@ -66,23 +67,22 @@ std::string GetCurrentSerialNumber() {
         return serialNumber;
     }
 
-    // Query the serial number buffer
     DeviceIoControl(hDevice, MY_DEVICE_IOCTL, NULL, 0, lpBuffer, dwSize, &dwSize, NULL);
 
-    // Get the serial number
+
     serialNumber = lpBuffer;
 
-    // Free the buffer
+
     delete[] lpBuffer;
 
-    // Close the device handle
+
     CloseHandle(hDevice);
 
-    // Return the serial number
+
     return serialNumber;
 }
 
-// This function spoofs the serial number of the current hardware
+
 bool SpoofSerialNumber(std::string serialNumber) {
 
     HANDLE hDevice;
@@ -91,42 +91,40 @@ bool SpoofSerialNumber(std::string serialNumber) {
     char inputBuffer[1024];
     DWORD dwBytes;
 
-    // Copy the serial number to the input buffer
+
     strcpy_s(inputBuffer, serialNumber.c_str());
 
-    // Open the device handle to the device
     hDevice = CreateFile(DEVICE_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
     if (hDevice == INVALID_HANDLE_VALUE) {
-        // Could not open device
+
         return false;
     }
 
-    // Execute the spoof control code
+
     DeviceIoControl(hDevice, MY_IOCTL_SPOOF, inputBuffer, serialNumber.length() + 1, NULL, 0, &dwBytes, (LPOVERLAPPED)NULL);
 
-    // Close the device handle
+
     CloseHandle(hDevice);
 
-    // Return success
+
     return true;
 }
 
-// Main entry point
 int main(int argc, char* argv[]) {
-    // Find the device name
+
     std::wstring deviceName = FindDeviceName();
     if (deviceName.empty()) {
         std::cerr << "Error: Could not find device name." << std::endl;
         return 1;
     }
 
-    // Get the current serial number
+  
     std::string currentSerial = GetCurrentSerialNumber();
     if (!currentSerial.empty()) {
-        // Spoof the serial number
+
         SpoofSerialNumber(currentSerial);
 
-        // Display success message
+
         std::cout << "Serial number spoofed successfully." << std::endl;
         std::cout << "Old HWID: " << currentSerial << std::endl;
         std::cout << "New HWID: " << GetCurrentSerialNumber() << std::endl;
